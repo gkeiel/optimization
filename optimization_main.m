71a83451b6882829a86cc567956b36310132e590
Guilherme Keiel <gkeiel@hotmail.com>
2025-08-15 10:09:03 -0300

    v1.0.0

--- optimization_main.m
+++ optimization_main.m
@@ -19,13 +19,34 @@ k_max = 20;
 
 %%%%%%%%%%%%%%%%%%%%%%% slect optimization solver %%%%%%%%%%%%%%%%%%%%%%%%%
 % solve for x
-[ x, f ] = gradient_descent( F, Gr, x_0, k_max );
+%[ x, f ] = gradient_descent( F, Gr, x_0, k_max );
 %[ x, f ] = hill_climbing( F, -x_0, k_max );
 [ x, f ] = simulated_annealing( F, x_0, k_max );
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% evolution of the objective function
+figure(1);
 plot(0:length(f)-1, f, 'LineWidth',2.5, 'color','k');
 title('Objective function evolution');
 xlabel('Iteration');
 ylabel('F(x)');
 grid on;
+
+if length(x) == 2
+    % create grid
+    x_1 = -5:0.1:5;
+    x_2 = -5:0.1:5;
+    [X1, X2] = meshgrid(x_1, x_2);
+    Z = arrayfun(@(a, b) F([a; b]), X1, X2);
+    
+    % 3D surface plot
+    figure(2);
+    surf(X1, X2, Z);
+    title('Surface plot of F(x)');
+    xlabel('x_1');
+    ylabel('x_2');
+    zlabel('F(x_1, x_2)');
+    grid on;
+    hold on;
+    plot3(x(1),x(2), F(x), 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
+end
